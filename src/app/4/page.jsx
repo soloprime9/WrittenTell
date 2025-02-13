b609d9ba2808b23f2d92eb382db31ccadef8da78
soloprime9 <primeindia5750@gmail.com>
2025-02-13 16:25:12 +0530

    GoodTel

---
+++ src/app/4/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Blog = () => {
+
+    return (
+        <div>
+            <h1>First Blog Here</h1>
+            <p>Novmber is here now</p>
+        </div>
+    )
+}
+
+export default Blog;
